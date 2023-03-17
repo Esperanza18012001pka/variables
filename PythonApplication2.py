406df11a4709323d991f7a100d90324e09ab163d
Acer <Acer@DESKTOP-A39HVKB>
2023-03-17 21:28:06 +0200

    Add project files.

---
+++ PythonApplication2.py
@@ -0,0 +1,15 @@
+from base64 import encode
+import encodings
+
+
+print(" Nothing \n will work \n unless you do")
+print("Anyone \n who stops learning\n   is old, whether at twenty\n     or eighty. Henry Ford")
+
+num_1 = int(input("Input number 1: "))
+
+num_2 = int(input("Input number 2: "))
+
+print(f"Result: { num_1 + num_2}")
+print(f"Result: { num_1 - num_2}")
+print(f"Result: { num_1 * num_2}")
+
